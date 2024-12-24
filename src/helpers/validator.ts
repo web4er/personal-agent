69fef1ec5746ec94518b3b9188bdc80e0ec83000
EresDev <eresdev@hotmail.com>
2024-12-24 20:41:01 +0500

    refactor: remove redundant func to satisfy knip

--- src/helpers/validator.ts
+++ src/helpers/validator.ts
@@ -1,20 +1,5 @@
-import * as github from "@actions/github";
-import { Octokit } from "@octokit/rest";
 import { TransformDecodeCheckError, TransformDecodeError, Value, ValueError } from "@sinclair/typebox/value";
 import { Env, envSchema, envValidator, PluginSettings, pluginSettingsSchema, pluginSettingsValidator } from "../types";
-
-export async function returnDataToKernel(repoToken: string, stateId: string, output: object, eventType = "return-data-to-ubiquity-os-kernel") {
-  const octokit = new Octokit({ auth: repoToken });
-  return octokit.repos.createDispatchEvent({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    event_type: eventType,
-    client_payload: {
-      state_id: stateId,
-      output: JSON.stringify(output),
-    },
-  });
-}
 
 export function validateAndDecodeSchemas(rawEnv: object, rawSettings: object) {
   const errors: ValueError[] = [];
